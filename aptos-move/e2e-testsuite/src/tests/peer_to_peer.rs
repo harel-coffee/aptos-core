f9addc7d443f819218caae63799c70150b27f11e
vusirikala <satyasatya123456@gmail.com>
2025-08-06 16:50:07 -0700

    Move tests for orderless transactions

--- aptos-move/e2e-testsuite/src/tests/peer_to_peer.rs
+++ aptos-move/e2e-testsuite/src/tests/peer_to_peer.rs
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use aptos_language_e2e_tests::{
-    account::Account, common_transactions::peer_to_peer_txn, current_function_name,
+    account::Account, common_transactions::peer_to_peer_txn,
     executor::FakeExecutor,
 };
 use aptos_types::{
